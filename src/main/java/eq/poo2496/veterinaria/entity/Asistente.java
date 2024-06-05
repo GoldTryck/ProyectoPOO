ffbffe36463b876c6d7b3f70480283f30114795f
EsmeraldaFloresHarrison <rosy17.floresh@gmail.com>
2024-06-05 09:34:03 -0600

    Commit en Asistente y Cita

--- src/main/java/eq/poo2496/veterinaria/entity/Asistente.java
+++ src/main/java/eq/poo2496/veterinaria/entity/Asistente.java
@@ -1,5 +1,6 @@
 package eq.poo2496.veterinaria.entity;
 
+// Importaciones necesarias para las anotaciones de JPA y Lombok
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -7,10 +8,12 @@ import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@Data
-@Entity
-@Table(name = "tAsistente")
-@EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
-@AttributeOverride(name = "id", column = @Column(name = "numeroAsistente"))
-public class Asistente extends Persona{
+@Data // Anotación de Lombok para generar automáticamente los métodos getter, setter, equals, hashCode, y toString
+@Entity // Anotación para indicar que esta clase es una entidad JPA
+@Table(name = "tAsistente") // Especificación del nombre de la tabla en la base de datos
+@EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true) // Anotación de Lombok para generar automáticamente
+                                                                    // los métodos equals y hashCode
+@AttributeOverride(name = "id", column = @Column(name = "numeroAsistente")) // Anotación para especificar que se desea
+                                                                            // sobrescribir el atributo de la clase base
+public class Asistente extends Persona{ // Definición de la clase Asistente, que extiende de la clase Persona
 }
--- src/main/java/eq/poo2496/veterinaria/entity/Cita.java
+++ src/main/java/eq/poo2496/veterinaria/entity/Cita.java
@@ -1,41 +1,42 @@
 package eq.poo2496.veterinaria.entity;
 
+// Importaciones necesarias para las anotaciones de JPA y Lombok
 import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.Date;
 import java.util.List;
 
-@Data
-@Table(name = "tCita")
-@Entity
-public class Cita {
+@Data // Anotación de Lombok para generar automáticamente los métodos getter, setter, equals, hashCode, y toString
+@Table(name = "tCita") // Especificación del nombre de la tabla en la base de datos
+@Entity // Anotación para indicar que esta clase es una entidad JPA
+public class Cita { // Definición de la clase Cita
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long numeroCita;
+    private long numeroCita; // Identificador de la cita, generado automáticamente
 
     @Temporal(TemporalType.TIMESTAMP)
     @Column(nullable = false, unique = true)
-    private Date fechaHora;
+    private Date fechaHora; // Fecha y hora de la cita
 
     @ManyToOne(optional = false)
     @JoinColumn(name = "numeroCliente")
-    private Cliente cliente;
+    private Cliente cliente; // Cliente asociado a la cita
 
     @ManyToOne(optional = false)
     @JoinColumn(name = "numeroMascota")
-    private Mascota mascota;
+    private Mascota mascota; // Mascota asociada a la cita
 
     @ManyToOne
     @JoinColumn(name = "numeroVeterinario")
-    private Veterinario veterinario;
+    private Veterinario veterinario; // Veterinario asociado a la cita
 
     @ManyToOne
     @JoinColumn(name = "numeroAsistente")
-    private Asistente asistente;
+    private Asistente asistente; // Asistente asociado a la cita
 
     @Column(nullable = false)
-    private String descripcionServicio;
+    private String descripcionServicio; // Descripción del servicio de la cita
 
     @ManyToMany
     @JoinTable(
@@ -43,5 +44,5 @@ public class Cita {
             joinColumns = @JoinColumn(name = "numeroCita"),
             inverseJoinColumns = @JoinColumn(name = "idPaquete")
     )
-    private List<Paquete> paquetes;
+    private List<Paquete> paquetes;// Lista de paquetes asociados a la cita
 }
