6a5b65a9231fbb75e46ab06790dbd1e8c9bcd2cd
EsmeraldaFloresHarrison <rosy17.floresh@gmail.com>
2024-06-05 08:28:18 -0600

    Commit en Main

--- src/main/java/eq/poo2496/veterinaria/controller/MainController.java
+++ src/main/java/eq/poo2496/veterinaria/controller/MainController.java
@@ -1,5 +1,11 @@
+/*Author: Larios Ponce Hector
+Description: Este código es un controlador JavaFX para una aplicación de veterinaria. Proporciona métodos para cargar
+vistas de mascotas y clientes desde archivos FXML, así como un método para cerrar la aplicación. Además, utiliza Spring
+Framework para la gestión de componentes.*/
+
 package eq.poo2496.veterinaria.controller;
 
+// Importaciones necesarias para el funcionamiento del controlador
 import eq.poo2496.veterinaria.util.SpringFXMLLoader;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,19 +19,22 @@ import java.util.Objects;
 import static eq.poo2496.veterinaria.Main.context;
 
 @Component
-public class MainController {
+public class MainController { // Define la clase MainController en el paquete eq.poo2496.veterinaria.controller
 
     @FXML
-    public VBox primaryPane;
+    public VBox primaryPane; // Declara un campo primaryPane de tipo VBox para el contenedor principal de la interfaz
+                             // de usuario
     @FXML
-    public HBox dataPane;
-    SpringFXMLLoader loader = new SpringFXMLLoader();
+    public HBox dataPane; // Declara un campo dataPane de tipo HBox para un contenedor de datos en la interfaz de usuario
+    SpringFXMLLoader loader = new SpringFXMLLoader(); // Crea una instancia de SpringFXMLLoader para cargar archivos FXML
     @FXML
-    private void MascotaView() throws IOException {
+    private void MascotaView() throws IOException { // Método privado que carga la vista de la mascota desde un archivo
+                                                    // FXML y la coloca en primaryPane
         loader.loadView("/eq/poo2496/veterinaria/mascotaView.fxml", primaryPane);
     }
     @FXML
-    private void ClienteView() throws IOException {
+    private void ClienteView() throws IOException { // Método privado que carga la vista del cliente desde un archivo
+                                                    // FXML y la coloca en primaryPane
         loader.loadView("/eq/poo2496/veterinaria/clienteView.fxml", primaryPane);
     }
 
@@ -33,5 +42,5 @@ public class MainController {
     @FXML
     public void endApp() {
         System.exit(0);
-    }
+    } // Método público que termina la aplicación
 }
