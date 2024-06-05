e12c375cbf949e727da7a4d853670a7799293b78
EsmeraldaFloresHarrison <rosy17.floresh@gmail.com>
2024-06-05 09:48:17 -0600

    Commit en Cliente y Gerente

--- src/main/java/eq/poo2496/veterinaria/entity/Cliente.java
+++ src/main/java/eq/poo2496/veterinaria/entity/Cliente.java
@@ -1,21 +1,35 @@
+/*Author: Larios Ponce Hector
+Description: Este código define la entidad Cliente en una aplicación de veterinaria. La clase Cliente hereda de la clase
+Persona, lo que significa que tiene todos los atributos y métodos de una persona además de los específicos de un cliente.
+Esta clase mapea a la tabla "tCliente" en la base de datos.*/
 package eq.poo2496.veterinaria.entity;
 
+// Importaciones necesarias para JPA y Lombok
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@Data
-@Entity
-@Table(name = "tCliente")
+@Data // La anotación @Data de Lombok genera automáticamente getters, setters, toString, equals y hashCode para la clase
+@Entity // Indica que esta clase es una entidad de JPA y se mapeará a una tabla en la base de datos
+@Table(name = "tCliente") // Define el nombre de la tabla en la base de datos
+
+// Lombok genera un método equals y hashCode basado solo en los campos explícitamente incluidos.
+// En este caso, solo se incluirá el campo 'id' de la clase base Persona.
 @EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
+
+// Permite realizar un override en la definición del atributo 'id' heredado de la clase Persona
 @AttributeOverride(name = "id", column = @Column(name = "numeroCliente"))
 public class Cliente extends Persona {
 
+    // Relación uno a uno con la entidad Mascota, con una cascada de operaciones que se aplican a la mascota
     @OneToOne(cascade = CascadeType.ALL)
+    // Define la columna que actuará como clave foránea en la tabla cliente para la relación con la mascota
     @JoinColumn(name = "numeroMascota", referencedColumnName = "numeroMascota", unique = true, nullable = false)
     private Mascota mascota;
 
+    // Relación uno a uno con la entidad Tarjeta, con una cascada de operaciones que se aplican a la tarjeta
     @OneToOne(cascade = CascadeType.ALL)
+    // Define la columna que actuará como clave foránea en la tabla cliente para la relación con la tarjeta
     @JoinColumn(name = "idTarjeta", referencedColumnName = "idTarjeta")
     private Tarjeta tarjeta;
 }
--- src/main/java/eq/poo2496/veterinaria/entity/Gerente.java
+++ src/main/java/eq/poo2496/veterinaria/entity/Gerente.java
@@ -1,5 +1,9 @@
+/*Author: Larios Ponce Hector
+Description: Este código define la entidad Gerente en una aplicación de veterinaria. La clase Gerente hereda de la clase
+Persona, lo que significa que tiene todos los atributos y métodos de una persona además de los específicos de un gerente.*/
 package eq.poo2496.veterinaria.entity;
 
+// Importaciones necesarias para JPA y Lombok
 import eq.poo2496.veterinaria.enums.Sucursal;
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
@@ -8,13 +12,19 @@ import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@Data
-@Entity
-@Table(name = "tGerente")
+@Data // La anotación @Data de Lombok genera automáticamente getters, setters, toString, equals y hashCode para la clase
+@Entity // Indica que esta clase es una entidad de JPA y se mapeará a una tabla en la base de datos
+@Table(name = "tGerente") // Define el nombre de la tabla en la base de datos
+
+// Lombok genera un método equals y hashCode basado solo en los campos explícitamente incluidos.
+// En este caso, solo se incluirá el campo 'id' de la clase base Persona.
 @EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
+// Permite realizar un override en la definición del atributo 'id' heredado de la clase Persona
 @AttributeOverride(name = "id", column = @Column(name = "numeroGerente"))
 public class Gerente extends Persona {
 
+    // Define una columna llamada 'sucursal' en la tabla tGerente.
+    // El valor de la columna no puede ser nulo y debe ser único en la tabla.
     @Column(name = "sucursal", nullable = false, unique = true)
     private Sucursal sucursal;
 }
